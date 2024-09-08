7545b58479d531a9210919af449444f1ca0827c1
anima-regem <vichukartha@gmail.com>
2024-09-08 05:47:21 +0530

    Initial backend UI implementation

---
+++ backend/main.py
@@ -0,0 +1,17 @@
+import asyncio
+from hume import HumeVoiceClient, MicrophoneInterface
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+async def main() -> None:
+  # Paste your Hume API key here
+  HUME_API_KEY = os.getenv("HUME_API_KEY")
+  # Connect and authenticate with Hume
+  client = HumeVoiceClient(HUME_API_KEY)
+
+  # Start streaming EVI over your device's microphone and speakers
+  async with client.connect(config_id=os.getenv("CONFIG_ID")) as socket:
+      await MicrophoneInterface.start(socket)
+asyncio.run(main())
